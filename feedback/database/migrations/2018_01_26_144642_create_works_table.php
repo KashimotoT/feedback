0596105811b9e34c54e1a20e4d6c669ab2463a52
岸野佑哉 <kishino@itto-inc.net>
2018-01-26 15:39:41 +0900

    migrationファイルの修正

--- feedback/database/migrations/2018_01_26_144642_create_works_table.php
+++ feedback/database/migrations/2018_01_26_144642_create_works_table.php
@@ -15,7 +15,7 @@ class CreateWorksTable extends Migration
     {
         Schema::create('works', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('auther')
+            $table->string('auther');
             $table->string('title');
             $table->timestamps();
         });
