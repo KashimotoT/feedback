42e14b5bd1e0b82a1fa90339263f03653eb230d4
岸野佑哉 <kishino@itto-inc.net>
2018-02-07 15:56:49 +0900

    [add]Workのindexを実装
    登録作品一覧ページを作成

---
+++ feedback/resources/views/works/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.default')
+
+@section('content')
+<h1>登録作品一覧</h1>
+<div class="table-resposive">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>著者</th>
+                <th>タイトル</th>
+                <th>登録日</th>
+                <th>更新日</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($works as $work)
+                <tr>
+                    <td>
+                        {{ $work->author }}
+                    </td>
+                    <td>
+                        <a href="{{ url('works/'.$work->id) }}">{{ $work->title }}</a>
+                    </td>
+                    <td>
+                        {{ $work->created_at }}
+                    </td>
+                    <td>
+                        {{ $work->ypdated_at }}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
