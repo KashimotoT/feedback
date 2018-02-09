5c9fd3d3033061417cb4e9c14db5eef8d6bb3a02
岸野佑哉 <kishino@itto-inc.net>
2018-02-09 17:21:12 +0900

    [update]User詳細に作品情報を表示
    ユーザー詳細画面にそのユーザーの作品一覧を追加しました

--- feedback/resources/views/users/show.blade.php
+++ feedback/resources/views/users/show.blade.php
@@ -24,4 +24,39 @@
     <dt class="col-md-2">メール</dt>
     <dd class="col-md-10">{{ $user->email }}</dd>
 </dl>
+
+<!-- 登録作品一覧 -->
+<h2>登録作品</h2>
+<div class="table-responsive">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>タイトル</th>
+                <th>登録日</th>
+                <th>更新日</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($user->work as $work)
+            <tr>
+                <td>
+                    <a href="{{ url('works/'.$work->id) }}">
+                        {{ $work->title }}</a>
+                </td>
+                <td>{{ $work->created_at }}</td>
+                <td>{{ $work->updated_at }}</td>
+                <td nowrap>
+                    <a href="{{ url('works/'.$work->id.'/edit') }}" class="btn btn-primary">編集</a>
+                    @component('components.btn-del')
+                        @slot('table', 'works')
+                        @slot('id', $work->id)
+                    @endcomponent
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+
 @endsection
